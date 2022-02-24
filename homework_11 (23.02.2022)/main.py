dcb982f61f627b2883d79f8a642900d3d6a45937
BelevichAndr <belevich00@gmail.com>
2022-02-24 23:04:53 +0300

    update homework_11

---
+++ homework_11 (23.02.2022)/main.py
@@ -0,0 +1,8 @@
+from matrix_package.matrix_classes import Matrix
+from matrix_package.matrix_funcs import *
+
+main_matrix = Matrix(n=10, m=10, a=0, b=9)
+print(main_matrix,)
+matrix_max_elem(main_matrix)
+matrix_min_elem(main_matrix)
+matrix_total_sum(main_matrix)
