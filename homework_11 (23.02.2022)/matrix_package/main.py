953c5fa9283118f3e5b1cfa734994427fcbcd0fc
BelevichAndr <belevich00@gmail.com>
2022-02-24 23:07:24 +0300

    delete main.py from package

--- homework_11 (23.02.2022)/matrix_package/main.py
+++
@@ -1,8 +0,0 @@
-from matrix_classes import Matrix
-from matrix_funcs import *
-
-main_matrix = Matrix(n=10, m=10, a=0, b=9)
-print(main_matrix,)
-matrix_max_elem(main_matrix)
-matrix_min_elem(main_matrix)
-matrix_total_sum(main_matrix)
