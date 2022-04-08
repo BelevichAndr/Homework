17ef724fa94de0a92f4a1efa5623872fefa78d0e
BelevichAndr <belevich00@gmail.com>
2022-04-08 21:58:53 +0300

    added homework 18

--- homework_19 (04.042022)/django_with_rest/django_with_rest/settings.py
+++ homework_19 (04.042022)/django_with_rest/django_with_rest/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_with_rest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- homework_19 (04.042022)/django_with_rest/first_app/views.py
+++ homework_19 (04.042022)/django_with_rest/first_app/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 from rest_framework.response import Response
 from .models import Tasks
 from rest_framework.views import APIView
+from .forms import TasksForm
 
 
 class TasksApi(APIView):
@@ -13,3 +16,25 @@ class TasksApi(APIView):
         add_task = Tasks.objects.create(**request.data)
         data_list = list(Tasks.objects.all().values())
         return Response({"tasks": data_list})
+
+
+class HomeWorkView(View):
+
+    def get(self, request):
+        form = TasksForm()
+        context = {
+            "form": form
+        }
+        return render(request, "form.html", context=context)
+
+    def post(self, request):
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            string = ""
+            for key in form.cleaned_data:
+                string += f"{form.cleaned_data.get(key)}|"
+            print(string)
+            return HttpResponse("Есть контакт")
+        return form.errors
+
+
