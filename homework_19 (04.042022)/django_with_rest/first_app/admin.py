6927eae5257a5bfd55d91366eba476dc2ef115c6
BelevichAndr <belevich00@gmail.com>
2022-04-14 21:23:09 +0300

    added homework 22

--- homework_19 (04.042022)/django_with_rest/first_app/admin.py
+++ homework_19 (04.042022)/django_with_rest/first_app/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Tasks
+from .models import *
 
 
 class TasksAdmin(admin.ModelAdmin):
     list_display = ("title", )
+
+
+@admin.register(Student)
+class RegisterInfo(admin.ModelAdmin):
+    list_display = ("firstname", "lastname")
+
+
+@admin.register(Book)
+class RegisterInfo(admin.ModelAdmin):
+    list_display = ("title",)
 
 
 admin.site.register(Tasks, TasksAdmin)
--- homework_19 (04.042022)/django_with_rest/first_app/models.py
+++ homework_19 (04.042022)/django_with_rest/first_app/models.py
@@ -16,3 +16,20 @@ class Customer(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     age = models.PositiveIntegerField()
+
+
+class Student(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.firstname}, {self.lastname}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    page_count = models.PositiveIntegerField()
+    student = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.title
